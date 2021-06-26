d04b8d0c4732f1d51b355de20ef0a18308b42e1f
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-06-26 23:55:04 +0300

    adding forms for Flask

--- main.py
+++ main.py
@@ -64,30 +64,30 @@ def index():
 
 @app.route('/ship', methods=['GET', 'POST'])  # Reads the 'value' of the button press which contains the ship name
 def ship():
-    ship_name = request.form["button"]
+    ship_name = request.form["button"]  # reads the value in button with name "button"
     if 'Bro Nibe' in ship_name:
         page = 'https://www.vesselfinder.com/vessels/BRO-NIBE-IMO-9322700-MMSI-220495000'
-        return redirect(windy_link(page))  # immediately redirects to weather_link returned by windy_link()
+        return redirect(windy_link(page))  # redirects to weather_link returned by windy_link()
         pass
     elif 'Bro Nissum' in ship_name:
         page = 'https://www.vesselfinder.com/vessels/BRO-NISSUM-IMO-9340623-MMSI-220504000'
-        return redirect(windy_link(page))  # immediately redirects to weather_link returned by windy_link()
+        return redirect(windy_link(page))
         pass
     elif 'Bro Agnes' in ship_name:
         page = 'https://www.vesselfinder.com/vessels/BRO-AGNES-IMO-9348302-MMSI-564057000'
-        return redirect(windy_link(page))  # immediately redirects to weather_link returned by windy_link()
+        return redirect(windy_link(page))
         pass
     elif 'Maersk Maru' in ship_name:
         page = 'https://www.vesselfinder.com/vessels/MAERSK-MARU-IMO-9581447-MMSI-563087300'
-        return redirect(windy_link(page))  # immediately redirects to weather_link returned by windy_link()
+        return redirect(windy_link(page))
         pass
     elif 'Maersk Cebu' in ship_name:
         page = 'https://www.vesselfinder.com/vessels/MAERSK-CEBU-IMO-9786176-MMSI-219393000'
-        return redirect(windy_link(page))  # immediately redirects to weather_link returned by windy_link()
+        return redirect(windy_link(page))
         pass
     elif 'Maersk Kaya' in ship_name:
         page = 'https://www.vesselfinder.com/vessels/MAERSK-KAYA-IMO-9431288-MMSI-566030000'
-        return redirect(windy_link(page))  # immediately redirects to weather_link returned by windy_link()
+        return redirect(windy_link(page))
         pass
 
 
